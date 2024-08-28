433393dc3865416be593fe3558042562a52246b0
christophechr <christophe.chhor@epitech.eu>
2024-08-28 14:19:59 +0200

    Swap img and Image balise being next compliant in socials component

--- src/app/contact/view/socials.tsx
+++ src/app/contact/view/socials.tsx
@@ -1,11 +1,12 @@
 import { Social } from './card/socialCard';
+import Image from 'next/image'
 
 export function Socials() {
     return (
         <div className="contact flex flex-col min-h-[100dvh]">
             <section id="contact" className="w-full flex-col justify-center items-center md:py-24 lg:py-32">
                 <Social href="https://github.com/christophechr" imageUrl="/icons/github.png" altName="GitHub Icon" width={50} height={50}/>
-                <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=christophechr&langs_count=8&theme=omni&layout=compact"/>
+                <Image src="https://github-readme-stats.vercel.app/api/top-langs/?username=christophechr&langs_count=8&theme=omni&layout=compact" alt="Github stats"/>
 
                 <Social href="https://linkedin.com/in/christophe-chhor" imageUrl="/icons/linkedin.png" altName="LinkedIn Icon" width={50} height={50}/>
 
