ebcbdbf4301c96da9ded1bd91bbf682b72fa1b9e
christophechr <christophe.chhor@epitech.eu>
2024-08-28 14:21:43 +0200

    Update the view of the skills

--- src/components/view/skills.tsx
+++ src/components/view/skills.tsx
@@ -16,9 +16,9 @@ export function Skills() {
             <div className="mx-auto grid max-w-5xl grid-cols-2 gap-6 py-12 sm:grid-cols-3 lg:grid-cols-4 lg:gap-12">
                 <Skill skillName="React" imageUrl="/icons/react.png" altName="React Icon" desc="A JavaScript library for building user interfaces." width={50} height={50}/>
                 <Skill skillName="Next.js" imageUrl="/icons/nextjs.png" altName="Next.js Icon" desc="A React framework for building server-rendered applications." width={50} height={50}/>
-                <Skill skillName="C++" imageUrl="/icons/cplusplus.png" altName="C++ Icon" desc="A React framework for building server-rendered applications." width={50} height={50}/>
+                <Skill skillName="Node.js" imageUrl="/icons/nodejs.png" altName="Node.js Icon" desc="A JavaScript runtime built on Chrome's V8 JavaScript engine, handling the server of websites." width={50} height={50}/>
+                <Skill skillName="C++" imageUrl="/icons/cplusplus.png" altName="C++ Icon" desc="One of the most powerful object-oriented programming language, mostly used in Video Games." width={50} height={50}/>
                 <Skill skillName="Java" imageUrl="/icons/java.png" altName="Java Icon" desc="Object-oriented programming language suitable for apps and backend development." width={50} height={50}/>
-                <Skill skillName="Node.js" imageUrl="/icons/nodejs.png" altName="Node.js Icon" desc="A JavaScript runtime built on Chrome's V8 JavaScript engine." width={50} height={50}/>
             </div>
         </div>
         </section>
