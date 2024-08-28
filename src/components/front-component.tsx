c65e347386545dc4f1f90a83b89ba7fbcabdf143
christophechr <christophe.chhor@epitech.eu>
2024-08-28 13:40:20 +0200

    ADD footer component and the terms of service's page

--- src/components/front-component.tsx
+++ src/components/front-component.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link"
-
 import { Profile } from "./view/profile"
 import { Projects } from "./view/projects"
 import { Skills } from "./view/skills"
+import { Footer } from "./view/footer"
 
 export function FrontComponent() {
   return (
@@ -18,18 +17,7 @@ export function FrontComponent() {
 
       </main>
 
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 Christophe CHHOR. All rights reserved.</p>
-
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link href="/policies" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-            Privacy Policy
-          </Link>
-          <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-            Terms of Service
-          </Link>
-        </nav>
-      </footer>
+      <Footer />
     </div>
   )
 }
