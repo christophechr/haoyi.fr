16ac126ce068d31c1ba0f62b92a7f1ae623f1b95
christophechr <christophe.chhor@epitech.eu>
2024-08-28 13:47:37 +0200

    Remove commented line generated by ESLint in priojects component for the main page

--- src/components/view/projects.tsx
+++ src/components/view/projects.tsx
@@ -8,7 +8,6 @@ export function Projects() {
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">My Projects</h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  {/* eslint-disable-next-line react/no-unescaped-entities */}
                   Check out some of the web applications I've built to showcase my skills.
                 </p>
               </div>
