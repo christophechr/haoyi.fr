62464a71106a729aa347128c6d4d6b44a9b34367
christophechr <christophe.chhor@epitech.eu>
2024-10-09 13:56:56 +0800

    Add url to the technology in the skill cards

--- src/components/view/card/skill.tsx
+++ src/components/view/card/skill.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 
-export function Skill(props: {skillName: string, imageUrl: string, altName: string, desc: string, width: number, height: number}) {
+export function Skill(props: {skillName: string, skillUrl: string, imageUrl: string, altName: string, desc: string, width: number, height: number}) {
     return (
         <div className="rounded-lg border bg-background p-4">
             <div className="flex flex-col items-center">
                 <div className="flex items-center justify-center">
-                    <Image src={props.imageUrl} alt={props.altName} width={props.width} height={props.height}/>
+                    <a href={props.skillUrl}>
+                        <Image src={props.imageUrl} alt={props.altName} width={props.width} height={props.height}/>
+                    </a>
                 </div>
                 <div className="mt-4 text-center">
-                    <h3 className="text-xl font-bold">{props.skillName}</h3>
+                    <a href={props.skillUrl}>
+                        <h3 className="text-xl font-bold">{props.skillName}</h3>
+                    </a>
                     <p className="text-muted-foreground text-center">{props.desc}</p>
                 </div>
             </div>
--- src/components/view/skills.tsx
+++ src/components/view/skills.tsx
@@ -14,11 +14,11 @@ export function Skills() {
             </div>
             </div>
             <div className="mx-auto grid max-w-5xl grid-cols-2 gap-6 py-12 sm:grid-cols-3 lg:grid-cols-4 lg:gap-12">
-                <Skill skillName="React" imageUrl="/icons/react.png" altName="React Icon" desc="A JavaScript library for building user interfaces." width={50} height={50}/>
-                <Skill skillName="Next.js" imageUrl="/icons/nextjs.png" altName="Next.js Icon" desc="A React framework for building server-rendered applications." width={50} height={50}/>
-                <Skill skillName="Node.js" imageUrl="/icons/nodejs.png" altName="Node.js Icon" desc="A JavaScript runtime built on Chrome's V8 JavaScript engine, handling the server of websites." width={50} height={50}/>
-                <Skill skillName="C++" imageUrl="/icons/cplusplus.png" altName="C++ Icon" desc="One of the most powerful object-oriented programming language, mostly used in Video Games." width={50} height={50}/>
-                <Skill skillName="Java" imageUrl="/icons/java.png" altName="Java Icon" desc="Object-oriented programming language suitable for apps and backend development." width={50} height={50}/>
+                <Skill skillName="React" skillUrl="https://react.dev" imageUrl="/icons/react.png" altName="React Icon" desc="A JavaScript library for building user interfaces." width={50} height={50}/>
+                <Skill skillName="Next.js" skillUrl="https://nextjs.org" imageUrl="/icons/nextjs.png" altName="Next.js Icon" desc="A React framework for building server-rendered applications." width={50} height={50}/>
+                <Skill skillName="Node.js" skillUrl="https://nodejs.org" imageUrl="/icons/nodejs.png" altName="Node.js Icon" desc="A JavaScript runtime built on Chrome's V8 JavaScript engine, handling the server of websites." width={50} height={50}/>
+                <Skill skillName="C++" skillUrl="https://en.wikipedia.org/wiki/C%2B%2B" imageUrl="/icons/cplusplus.png" altName="C++ Icon" desc="One of the most powerful object-oriented programming language, mostly used in Video Games." width={50} height={50}/>
+                <Skill skillName="Java" skillUrl="https://docs.oracle.com/en/java/" imageUrl="/icons/java.png" altName="Java Icon" desc="Object-oriented programming language suitable for apps and backend development." width={50} height={50}/>
             </div>
         </div>
         </section>
