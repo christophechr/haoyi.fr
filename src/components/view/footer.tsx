72cbee9629171ed811019c8db0ac312b6b410c35
christophechr <christophe.chhor@epitech.eu>
2024-10-09 13:56:30 +0800

    Add dynamic update of the footer (update the year of the latest changelog of the website)

--- src/components/view/footer.tsx
+++ src/components/view/footer.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
 
 export function Footer() {
-    return (
+  const currentYear = new Date().getFullYear();
+  return (    
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 Christophe CHHOR. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground">&copy; {currentYear} Christophe CHHOR. All rights reserved.</p> 
 
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link href="/policies" className="text-xs hover:underline underline-offset-4" prefetch={false}>
